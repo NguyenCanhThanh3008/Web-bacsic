abfe4db330cd4204560ea4454a7d9df8fa3a789b
NguyenCanhThanh3008 <138221144+NguyenCanhThanh3008@users.noreply.github.com>
2023-07-01 17:15:17 +0700

    7/1

--- web_Thebrand_basi/buy-ticker.js
+++ web_Thebrand_basi/buy-ticker.js
@@ -3,23 +3,53 @@ const modal = document.querySelector(".js-modal")
 const modalContainer = document.querySelector(".js-modal-container")
 const modalClose = document.querySelector(".js-close-modal")
 
-function showBuyTicker(){
+function showBuyTicker() {
     modal.classList.add("open")
 
 }
 
-function closeBuyTicker(){
+function closeBuyTicker() {
     modal.classList.remove("open")
 
 }
 
-for(const btnBuy of btnBuys){
-    btnBuy.addEventListener("click",showBuyTicker)
+for (const btnBuy of btnBuys) {
+    btnBuy.addEventListener("click", showBuyTicker)
 }
 
-modalClose.addEventListener("click",closeBuyTicker)
+modalClose.addEventListener("click", closeBuyTicker)
 
-modal.addEventListener("click",closeBuyTicker)
-modalContainer.addEventListener("click",function(event){
+modal.addEventListener("click", closeBuyTicker)
+modalContainer.addEventListener("click", function (event) {
     event.stopPropagation()
 })
+
+let header = document.getElementById("header")
+let menu = document.getElementById("mobile-menu")
+let headerHeiight = header.clientHeight
+menu.onclick = function () {
+    let isClose = header.clientHeight === headerHeiight
+    if (isClose) {
+        header.style.height = "227px"
+    } else {
+        header.style.height = null
+
+    }
+}
+
+let menuheaders = document.querySelectorAll(".nav li a[href*='#']")
+for (let i = 0; i < menuheaders.length; i++) {
+    let menuheader = menuheaders[i]
+    menuheader.onclick = function (event) {
+        let moreHeader = this.nextElementSibling && this.nextElementSibling.classList.contains("subnav")
+        if (!moreHeader) {
+            header.style.height = null
+            
+        }else{
+            event.prevenDefault();
+            console.log("abc")
+
+        }
+
+    }
+}
